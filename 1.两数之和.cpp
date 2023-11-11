eb906e23c0971df0b2c9fa18facff74fd5d61d93
KingsleyCheng <chengzijie2021@outlook.com>
2023-11-11 15:29:21 +0800

    Update 1.两数之和.cpp

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -8,18 +8,20 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> Hashmap;
-        for(int i=0; i<nums.size(); ++i){
-            if(Hashmap.find(target-nums[i])==Hashmap.end())
-                Hashmap.insert(pair<int,int>(nums[i],i));
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> Hashmap;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (Hashmap.find(target - nums[i]) == Hashmap.end())
+                Hashmap.insert(pair<int, int>(nums[i], i));
             else
-                return vector<int>{Hashmap.find(target-nums[i])->second,i};
+                return vector<int>{Hashmap.find(target - nums[i])->second, i};
         }
-        return vector<int>{-1,-1};
+        return vector<int>{-1, -1};
     }
 };
 // @lc code=end
-
