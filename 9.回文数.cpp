e7437b41398251270afee60fd3cc9238e65bfaa9
KingsleyCheng <chengzijie2021@outlook.com>
2023-11-12 08:58:08 +0800

    Create 9.回文数.cpp

---
+++ 9.回文数.cpp
@@ -0,0 +1,23 @@
+// #include <string>
+// using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=9 lang=cpp
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        string s = to_string(x);
+        for (int i = 0, j = s.size() - 1; i < j; ++i, --j)
+            if (s[i] != s[j])
+                return false;
+        return true;
+    }
+};
+// @lc code=end
