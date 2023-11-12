43eaf5670b510f76838d2cc66dfdc1d8bb2e18c5
KingsleyCheng <chengzijie2021@outlook.com>
2023-11-12 14:27:16 +0800

    Update 3.无重复字符的最长子串.cpp

--- 3.无重复字符的最长子串.cpp
+++ 3.无重复字符的最长子串.cpp
@@ -1,6 +1,6 @@
-#include<string>
-#include<unordered_map>
-using  namespace std;
+#include <string>
+#include <unordered_map>
+using namespace std;
 /*
  * @lc app=leetcode.cn id=3 lang=cpp
  *
@@ -8,23 +8,27 @@ using  namespace std;
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int> hashmap;
-        int ans =0;
-        for(int i=0, j=0; i<s.size(); ++i){
-            if(hashmap.find(s[i]) == hashmap.end()){
-                j = max(0,j);
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<char, int> hashmap;
+        int ans = 0;
+        for (int i = 0, j = 0; i < s.size(); ++i)
+        {
+            if (hashmap.find(s[i]) == hashmap.end())
+            {
+                j = max(0, j);
             }
-            else{
-                j = max(hashmap.find(s[i])->second,j);
+            else
+            {
+                j = max(hashmap.find(s[i])->second, j);
             }
-            hashmap[s[i]] = i+1;
-            ans = max(ans, i-j+1);
+            hashmap[s[i]] = i + 1;
+            ans = max(ans, i - j + 1);
         }
         return ans;
     }
 };
 // @lc code=end
-
