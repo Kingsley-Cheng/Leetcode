27bb641b3dc93c94697ca1055ec3375db5f51e44
KingsleyCheng <chengzijie2021@outlook.com>
2023-12-04 14:36:47 +0800

    Update 28.找出字符串中第一个匹配项的下标.cpp

--- 28.找出字符串中第一个匹配项的下标.cpp
+++ 28.找出字符串中第一个匹配项的下标.cpp
@@ -1,4 +1,4 @@
-#include<string>
+#include <string>
 using namespace std;
 /*
  * @lc app=leetcode.cn id=28 lang=cpp
@@ -7,11 +7,27 @@ using namespace std;
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int strStr(string haystack, string needle) {
-
+    int strStr(string haystack, string needle)
+    {
+        int n = haystack.size(), m = needle.size();
+        for (int i = 0; i + m <= n; ++i)
+        {
+            bool flag = true;
+            for (int j = 0; j < m; ++j)
+            {
+                if (haystack[i + j] != needle[j])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+                return i;
+        }
+        return -1;
     }
 };
 // @lc code=end
-
